51e48fb24cc0f435309f3190cddd6689bf8139bd
李军辉 <junhui.li@southgis.com>
2021-04-13 20:20:19 +0800

    忽略路径更改

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,8 +1,8 @@
 /*
  * @Date: 2021-02-28 22:22:14
- * @LastEditors: zhou
- * @LastEditTime: 2021-04-07 23:43:32
- * @FilePath: /yfkj/src/api/baseUrl.js
+ * @LastEditors: junhui li
+ * @LastEditTime: 2021-04-13 20:19:04
+ * @FilePath: /yfkj/src/api/baseUrl.js11
  */
 //url要改为后端发布的公网链接
 //const baseUrl = 'http://172.16.20.46:3000'
