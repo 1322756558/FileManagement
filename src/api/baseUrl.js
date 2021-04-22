fa671a61e29ea9380d63dbb52a6b5143bfa6a155
zhou <1239704840@qq.com>
2021-04-22 17:03:35 +0800

    joj

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,10 +1,10 @@
 /*
  * @Date: 2021-02-28 22:22:14
  * @LastEditors: zhou
- * @LastEditTime: 2021-04-14 17:56:39
+ * @LastEditTime: 2021-04-22 15:19:26
  * @FilePath: \FileManagement\src\api\baseUrl.js
  */
-//url要改为后端发布的公网链接
+//url要改为后端发布的公网链接192.168.110.148
 
 const baseUrl = 'http://localhost:3000'
 
